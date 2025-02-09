927471c84d00ddad579a59695274b39de47c88fb
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-09 17:20:16 +0100

    finished exerciese

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+let result = ""
 
+if (num >= 0) {
+for(let i = 1; i <= num; i++) {
+ result += string 
+}
+}
+
+else if (num < 0) {
+    result = "ERROR"
+}
+return result
 };
 
 // Do not edit below this line
