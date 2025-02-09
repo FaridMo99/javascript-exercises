7bc366f0823989e00dfdca1ffd13261cfa82e1fa
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-09 20:16:21 +0100

    finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	const sum = a + b
+  return sum
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  const sum = a - b
+  return sum
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sum = 0;
+  a.forEach(num => sum += num);
+  return sum;
+
+}
+
+
+const multiply = function(a) {
+	let sum = 1
+  for (const num of a){
+    sum *= num
+  }
+  return sum
 };
 
-const multiply = function() {
+const power = function(a, b) {
+// a wird mit a multipliert und zwar so oft wie b ist
+let sum = 1;
+for(let i = b; i > 0; i--){
+ sum *= a
+}
+
+return sum
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) { 
+  let sum = 1; 
+  for (let i = 2; i <= a; i++) {
+      sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
