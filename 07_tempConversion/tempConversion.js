0b83d13e8f5b37ec4e41b67e248534d1cebc06b6
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-09 19:46:53 +0100

    finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  const fahrenheitToCelsius = (temp - 32) * 5/9;
+  const rounded = parseFloat(fahrenheitToCelsius.toFixed(1));
+
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const celsiusToFahrenheit = (temp * 9/5) + 32
+  const rounded = parseFloat(celsiusToFahrenheit.toFixed(1));
+
+  return rounded;
 };
 
 // Do not edit below this line
