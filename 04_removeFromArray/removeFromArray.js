72e2cad85e636722202b16fd9715ce1a30404ab9
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-09 18:51:53 +0100

    finished exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(a, b, c, d, e) {
 
+    const result = a.filter(number => number !== b && number !== c && number !== d && number !== e)
+
+    return result
+    
+};
+console.log(removeFromArray([1, 2, 3], 3))
 // Do not edit below this line
 module.exports = removeFromArray;
